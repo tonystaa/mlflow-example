856ea669439b5bca1f9eba0395d4ea336c933472
Mohammed Haris <haris@Mohammeds-MacBook-Air.local>
2021-08-06 12:07:27 +0530

    changes

--- train.py
+++ train.py
@@ -29,8 +29,11 @@ if __name__ == "__main__":
     np.random.seed(40)
 
     # Read the wine-quality csv file (make sure you're running this from the root of MLflow!)
-    wine_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "wine-quality.csv")
-    data = pd.read_csv(wine_path)
+    # wine_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "wine-quality.csv")
+
+    training_file_s3 = sys.argv[1]
+    data = pd.read_csv(training_file_s3)
+    print(data)
 
     # Split the data into training and test sets. (0.75, 0.25) split.
     train, test = train_test_split(data)
@@ -41,26 +44,34 @@ if __name__ == "__main__":
     train_y = train[["quality"]]
     test_y = test[["quality"]]
 
-    alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
-    l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
+    # alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
+    # l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
 
-    with mlflow.start_run():
-        lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
-        lr.fit(train_x, train_y)
+    alpha_range = np.arange(0, 2, 0.1)
+    l1_ratio_range = np.arange(0, 2, 0.1)
 
-        predicted_qualities = lr.predict(test_x)
+    print(mlflow.get_artifact_uri())
+    experiment_id = mlflow.set_experiment("training experiment")
 
-        (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+    for alpha in alpha_range:
+        for l1_ratio in l1_ratio_range:
+            with mlflow.start_run(experiment_id=experiment_id):
+                lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
+                lr.fit(train_x, train_y)
 
-        print("Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
-        print("  RMSE: %s" % rmse)
-        print("  MAE: %s" % mae)
-        print("  R2: %s" % r2)
+                predicted_qualities = lr.predict(test_x)
 
-        mlflow.log_param("alpha", alpha)
-        mlflow.log_param("l1_ratio", l1_ratio)
-        mlflow.log_metric("rmse", rmse)
-        mlflow.log_metric("r2", r2)
-        mlflow.log_metric("mae", mae)
+                (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
 
-        mlflow.sklearn.log_model(lr, "model")
+                print("Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
+                print("  RMSE: %s" % rmse)
+                print("  MAE: %s" % mae)
+                print("  R2: %s" % r2)
+
+                mlflow.log_param("alpha", alpha)
+                mlflow.log_param("l1_ratio", l1_ratio)
+                mlflow.log_metric("rmse", rmse)
+                mlflow.log_metric("r2", r2)
+                mlflow.log_metric("mae", mae)
+
+                mlflow.sklearn.log_model(lr, "model")
